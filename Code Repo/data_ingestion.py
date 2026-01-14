ef3770c51faa892ffb5bec342c76c463cb12b1fa
Amit-1997 <amitchaurasia1997.ac@gmail.com>
2026-01-14 09:53:58 +0530

    for checking

--- Code Repo/data_ingestion.py
+++ Code Repo/data_ingestion.py
@@ -17,14 +17,15 @@ raw_transactions= spark.read.format("json").option("multiline", True).load("/Use
 
 
 
+#cleaning related to the rides data
 rides= raw_ride_requested.withColumn("event_type", F.upper("event_type"))
 
 # due to some network issue in kafka it was not able to acknowledge the event and retries and due to which
 #event evt_12345 is getting duplicated so we need to deduplicate this event
 
-window = Window.partitionBy("event_id").orderBy("timestamp")
-rides= rides.withColumn("rnk", F.row_number().over(window)).filter(F.col("rnk") == 1).drop("rnk")
-rides_agg= rides.groupby("ride_id").agg(
+rides_window = Window.partitionBy("event_id").orderBy("timestamp")
+rides= rides.withColumn("rnk", F.row_number().over(rides_window)).filter(F.col("rnk") == 1).drop("rnk")
+clean_rides= rides.groupby("ride_id").agg(
     min(when(F.col("event_type")=="REQUESTED" , F.col("timestamp"))).alias("start_time"),
     max(when(F.col("event_type")=="COMPLETED", F.col("timestamp"))).alias("end_time"),
     max(F.col("user_id")).alias("user_id"),
@@ -34,14 +35,45 @@ rides_agg= rides.groupby("ride_id").agg(
 )
 
 # check if end_time is null means the ride is incomplete
-rides_agg = rides_agg.withColumn("status", when (F.col("end_time").isNull(), "INCOMPLETE") .otherwise("COMPLETED"))
+clean_rides = clean_rides.withColumn("status", when (F.col("end_time").isNull(), "INCOMPLETE") .otherwise("COMPLETED"))
+clean_rides.show()
+clean_rides.write.format("parquet").mode("overwrite").save("/Users/amitchaurasia/PycharmProjects/Rider-Hailing/Silver/clean_rides")
 
-rides_agg.write.format("parquet").save("/Users/amitchaurasia/PycharmProjects/Rider-Hailing/Silver/clean_rides")
+#cleaning related to the User data
+user_window = Window.partitionBy("user_id").orderBy(F.col("updated_at").desc())
+
+clean_user = (
+    raw_user
+    .withColumn("rn", F.row_number().over(user_window))
+    .filter(F.col("rn") == 1)
+    .drop("rn")
+    .select("user_id", "name", "phone", "email" , "rating" , "created_at")
+
+)
+clean_user.show()
+clean_user.write.format("parquet").mode("overwrite").save("/Users/amitchaurasia/PycharmProjects/Rider-Hailing/Silver/clean_user")
 
 
+#Driver Deduplication and standardize status
+driver_window = Window.partitionBy("driver_id").orderBy(F.col("updated_at").desc())
 
+clean_driver = (
+    raw_driver
+    .withColumn("rn", F.row_number().over(driver_window))
+    .filter(F.col("rn") == 1)
+    .drop("rn")
+    .select("driver_id", "name", "phone",F.upper(F.col("status")), "rating" , "vehicle_id", "created_at")
 
+)
+clean_driver.show()
+clean_driver.write.format("parquet").mode("overwrite").save("/Users/amitchaurasia/PycharmProjects/Rider-Hailing/Silver/clean_driver")
 
-
-
-
+#clean Vehicle
+clean_vehicle = (
+    raw_vehicles
+    .drop_duplicates(["vehicle_id"])
+    .withColumn("vehicle_type", F.upper(F.col("vehicle_type")))
+    .select("vehicle_id", "vehicle_type", "plate_number", "capacity")
+)
+clean_vehicle.show()
+clean_vehicle.write.format("parquet").mode("overwrite").save("/Users/amitchaurasia/PycharmProjects/Rider-Hailing/Silver/clean_vehicle")
