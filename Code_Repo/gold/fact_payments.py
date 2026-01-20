3d43466af346427748d5eea52b119ff8a7946dbf
Amit-1997 <amitchaurasia1997.ac@gmail.com>
2026-01-20 20:34:08 +0530

    Refractor file system

--- Code_Repo/gold/fact_payments.py
+++ Code_Repo/gold/fact_payments.py
@@ -2,7 +2,7 @@ from pyspark.sql import functions as F
 from Code_Repo.common.spark_session import get_spark
 from Code_Repo.common.utils import table_exist
 from Code_Repo.configs.paths import SILVER_PATH, GOLD_PATH
-from Code_Repo.gold.fact_rides import fact_rides
+
 
 
 def main():
@@ -23,7 +23,7 @@ def main():
             "paid_date"
         )
     )
-
+    fact_payments.show()
     fact_payments.write.mode("overwrite").parquet(f"{GOLD_PATH}/fact_payments")
 
     spark.stop()
